2b8b480f7c67796469d662e45118fbc59828dfc4
Aitor <amartinez346@ikasle.ehu.eus>
2019-02-08 10:08:03 +0100

    primer commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+def main():
+	print 'Hola mundo!'
+	#Vamos con operaciones básicas por si alguien quiere crear un branch para esa operción, como si su branch fuera trabajar con esa operación
+def subtract(x,y):
+	return x-y
+def multiply(x,y):
+	return x*y
+def divide(x,y):
+	return x/y
+def square(x,y):
+	pass
